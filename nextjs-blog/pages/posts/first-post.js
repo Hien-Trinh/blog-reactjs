7634678d639febc8accbc2c80c049075a75bb2e1
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-06-21 22:59:19 +0700

    Added Tailwind

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,15 +1,25 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 
 export default function FirstPost() {
     return (
-        <React.Fragment className="flex justify-center content-center">
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </React.Fragment>
+        <>
+            <Head>
+                <title>First Post</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="bg-violet-300">
+                <h1 className="text-center">First Post</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+                <Image src={"/images/profile_pic_1.jpeg"} height={516} width={516} alt="Your name"></Image>
+            </div>
+            <style></style>
+        </>
     );
 }
